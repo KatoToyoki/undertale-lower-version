b9cc57cb954273db49bec8f85571ceb3a5dd0832
TYK <b0984017369@gmail.com>
2023-03-09 22:33:34 +0800

    menu sketch

---
+++ Source/Game/Menu.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include "Menu.h"
+
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+
+void Stage::SetCoordinate(int x, int y) {
+    _coordinate.x = x;
+    _coordinate.y = y;
+}
+
+int Stage::GetID()
+{
+    return _ID;
+}
+
+std::string Stage::GetName()
+{
+    return _stageName;
+}
+
+int Stage::GetX()
+{
+    return _coordinate.x;
+}
+
+int Stage::GetY()
+{
+    return _coordinate.y;
+}
+
+// =======================================================
+
+void StartMenu::ChangeColor(Stage stage, int state) {
+    if (state == toOff) {
+        ChangeFontLog(stage._pDC, 21, "微軟正黑體",
+                                 RGB(color.off.r, color.off.g, color.off.b));
+    } 
+    else if (state == toOn) {
+        ChangeFontLog(stage._pDC, 21, "微軟正黑體",
+                                 RGB(color.on.r, color.on.g, color.on.b));
+    }
+}
+
+void StartMenu::SetUpStages() {
+    for (int i = 0; i < 3; i++) {
+        stages.push_back(Stage(i, "Stage" + std::to_string(i)));
+    }
+}
+
+void StartMenu::ShowStages() {
+    for (int i = 0; i < 3; i++) {
+        ChangeColor(stages[i], toOn);
+        stages[i].SetCoordinate(200*i,400);
+        Print(stages[i]._pDC, stages[i].GetX(), stages[i].GetY(), stages[i].GetName());
+    }
+}
+
+void StartMenu::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags){
+    if (nChar == VK_LEFT && currentStage != 0) {
+        ChangeColor(stages[currentStage], toOff);
+        currentStage -= 1;
+        ChangeColor(stages[currentStage], toOn);
+    }
+    else if (nChar == VK_RIGHT && currentStage != 3) {
+        ChangeColor(stages[currentStage], toOff);
+        currentStage += 1;
+        ChangeColor(stages[currentStage], toOn);
+    }
+}
---
+++ Source/Game/Menu.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#ifndef Menu
+#define Menu
+
+#include <string>
+#include <vector>
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+
+struct Coordinate {
+    int x, y;
+};
+
+struct rgbColor {
+    int r, g, b;
+};
+
+enum state { toOff, toOn };
+
+// ==========================================================
+
+class TextColor {
+private:
+protected:
+public:
+    rgbColor off, on;
+
+    TextColor() {
+        off.r = 255;
+        off.g = 255;
+        off.b = 255;
+        on.r = 252;
+        on.g = 252;
+        on.b = 45;
+    }
+};
+
+// ==========================================================
+
+class Stage {
+private:
+    int _ID;
+    Coordinate _coordinate;
+    std::string _stageName;
+
+protected:
+public:
+    CDC *_pDC;
+
+    // constructor
+    Stage(int ID, std::string name) {
+        _ID = ID;
+        _coordinate.x=0;
+        _coordinate.y=0;
+        _stageName = name;
+        _pDC = game_framework::CDDraw::GetBackCDC();
+    }
+
+    // destructor
+    ~Stage()
+    {
+        game_framework::CDDraw::ReleaseBackCDC();
+    }
+
+    // setter
+    void SetCoordinate(int x, int y);
+
+    // getter
+    int GetID();
+    std::string GetName();
+    int GetX();
+    int GetY();
+};
+
+// ==========================================================
+
+class StartMenu:public game_framework::CTextDraw{
+private:
+    int currentStage;
+    TextColor color;
+    std::vector<Stage> stages;
+
+    // to change color of text
+    void ChangeColor(Stage stage, int state);
+
+protected:
+public:
+    // constructor
+    StartMenu()
+    {
+        currentStage = 0;
+        SetUpStages();
+    }
+
+    // set up all the stages 
+    void SetUpStages();
+
+    // to reveal stages
+    void ShowStages();
+
+    // to get the key
+    void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+};
+
+#endif
