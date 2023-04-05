2a4343556587e7fc1bb8a39497fdf70f9f966829
TYK <b0984017369@gmail.com>
2023-04-05 18:37:54 +0800

    add comments

--- Source/Game/BarrageMode.h
+++ Source/Game/BarrageMode.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cstdlib>
 
+// to set init data ( resetting can use it as well)
 struct SetData
 {
     int damage=0;
@@ -18,10 +19,21 @@ struct SetData
 class BarrageMode
 {
 private:
+    // currently not be used right now, if it really don't use can deleted
     bool _enable;
+
+    // how many barrages the enemy attack mode will hava
     int _quantity=0;
+
+    // for random should check this
+    // it can check the ith barrage should placed in the ith x or y
+    // so that it won't many barrages in the same position
     int xPosition[50],yPosition[50];
+
+    // to put all barrages setting in this mode
     std::vector<Barrage> enemyBarrage;
+
+    // the init data (to be used with struct SetData)
     std::vector<SetData> allData;
 
 public:
@@ -30,14 +42,35 @@ public:
         _quantity=quantity;
     }
     ~BarrageMode()=default;
-    
+
+    // it will init everything you need,
+    // set xPosition, yPosition depends on each mode
+    // kind of virtual function
     void Init();
+
+    // it supposed to be pure virtual function
+    // because each mode have different condition
     void SetAllData();
+
+    // depends on quantity, push empty barrage in vector enemyBarrage
     void PushEmpty();
+
+    // set each barrage in vector enemyBarrage
     Barrage Setup(SetData data);
+
+    // you can randomly generate barrages
+    // should be virtual function MAYBE
     void RandomBarrage();
+
+    // generate barrages by allData order
     void NormalBarrage();
+
+    // show all barrages
     void ShowBarrage();
+
+    // close all barrages
     void UnshowBarrage();
+
+    // to let enemy attack you
     void MovingBarrage(Move *heart, int speed);
 };
--- Source/Game/mygame.h
+++ Source/Game/mygame.h
@@ -118,8 +118,6 @@ private:
   Menu menu;
   GameText game_text;
   ShowNormalMode show_normal_mode;
-  // Barrage barrage;
-  // Barrage boneRed1,boneRed2,boneRed3;
   BarrageMode boneRed=BarrageMode(3);
   MonsterFrame monster_frame;
   
